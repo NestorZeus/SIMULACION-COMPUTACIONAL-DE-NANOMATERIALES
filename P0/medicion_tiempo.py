4dda489e2bd1a477ff91f0bf73cf5f6b92370419
NestorZeus <98040575+NestorZeus@users.noreply.github.com>
2022-02-02 12:32:00 -0600

    Update medicion_tiempo.py

--- P0/medicion_tiempo.py
+++ P0/medicion_tiempo.py
@@ -13,25 +13,3 @@ def get_size(obj, seen=None):
     obj_id = id(obj)
     if obj_id in seen:
         return 0
-    # Important mark as seen *before* entering recursion to gracefully handle
-    # self-referential objects
-    seen.add(obj_id)
-    if isinstance(obj, dict):
-        size += sum([get_size(v, seen) for v in obj.values()])
-        size += sum([get_size(k, seen) for k in obj.keys()])
-    elif hasattr(obj, '__dict__'):
-        size += get_size(obj.__dict__, seen)
-    elif hasattr(obj, '__iter__') and not isinstance(obj, (str, bytes, bytearray)):
-        size += sum([get_size(i, seen) for i in obj])
-    return size
-
-for k in range(menor, mayor):
-    n = 2**k
-    lista = [ randint(desde, hasta) for i in range(n)]
-    antes = time()
-    lista.sort()
-    despues = time()
-    diferencia = despues - antes
-    print('ordenar', n, 'elementos toma', diferencia, 'segundos ',
-          '\n y en conjunto pesan', get_size(lista), 'bytes')
-print('bye')
