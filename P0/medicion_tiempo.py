2a7c4b8af328f8aec00a82da13a94ec250df6628
NestorZeus <98040575+NestorZeus@users.noreply.github.com>
2022-02-01 22:48:58 -0600

    Mediciones en Tiempos en Python
    Aprendiendo a crear tiempos y mediciones en programa Python

--- P0/medicion_tiempo.py
+++ P0/medicion_tiempo.py
@@ -1,12 +1,10 @@
 from random import randint
 from time import time
 import sys
-
 desde = 1
 hasta = 1000
 menor = 15
 mayor = 22
-
 def get_size(obj, seen=None):
     """Recursively finds size of objects"""
     size = sys.getsizeof(obj)
