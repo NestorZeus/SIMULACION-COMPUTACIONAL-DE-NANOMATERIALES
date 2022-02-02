ac65b946e247b212d2bf2da11b89dd1bbadf3dd7
NestorZeus <98040575+NestorZeus@users.noreply.github.com>
2022-02-01 22:29:41 -0600

    Update P0.py

--- P0/P0.py
+++ P0/P0.py
@@ -2,6 +2,15 @@ y=10
 x=13
 k=(y, x, 13)
 print(min(k))
-
-
-
+{
+lista = [x, y, z]
+datos = [a, b, c]
+M = np.matrix([lista, datos])
+suma = sum(lista)
+maximo = max(lista)
+minimo = min(lista)
+ecuacion = ((suma * maximo) + (minimo**3))
+estadistica = describe(lista)
+s = np.arange(1, 20, 3)
+aleatorio = rand(100)
+}
