b74c169e1fdefe0ada296c0d156410ce0f1a8375
NestorZeus <98040575+NestorZeus@users.noreply.github.com>
2022-02-02 12:30:31 -0600

    Update medicion_tiempo.R

--- P0/medicion_tiempo.R
+++ P0/medicion_tiempo.R
@@ -4,6 +4,3 @@ M = function(n) {k = 2^n; return(matrix(runif(k*k), nrow = k))}
 
 for (n in 5:10) 
 {
-cat('La matriz', n, 'con', 2^n, 'elementos tarda',
-system.time(M(n))[3], 'segundos en crearse y pesa', object_size(M(n)), 'bytes', '\n')
-}
