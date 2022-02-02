e709de506af404084aaaa6c65860e99a78deb284
NestorZeus <98040575+NestorZeus@users.noreply.github.com>
2022-02-02 12:33:10 -0600

    Update medicion_tiempo.R

--- P0/medicion_tiempo.R
+++ P0/medicion_tiempo.R
@@ -1,6 +1,6 @@
 library(pryr)
 
-M = function(n) {k = 2^n; return(matrix(runif(k*k), nrow = k))}
+M = function(n) {k = 15^n; return(matrix(runif(k*k), nrow = k))}
 
-for (n in 5:10) 
+for (n in 8:20) 
 {
